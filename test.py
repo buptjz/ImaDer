58828de88c032c6bea820d8dfd1bdfc8ae2b9a22
wangjizhe <buptjz@gmail.com>
2013-11-07 11:12:39 +0800

    add test for finals

---
+++ test.py
@@ -0,0 +1,18 @@
+a = ["1","2","3","4","5"]
+from copy import deepcopy
+
+def f(data_arr):
+    if x == 4:return [a[4]]
+    else:
+        for i in data_arr:
+            nextArray = deepcopy(data_arr)
+            data_arr
+            i + f(nextArray)
+        ret = f(x+1)
+        arr = []
+        for r in ret:
+            for temp in
+            arr.append(a[x]+r)
+        return arr
+    
+print(f(0))
